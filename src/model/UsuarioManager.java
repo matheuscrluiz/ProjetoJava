df715fc4e862c1c028b7e1cef1caffafe68e3bce
Matheus <matheuscrluiz@gmail.com>
2023-12-16 18:39:07 -0300

    feat: create project

---
+++ src/model/UsuarioManager.java
@@ -0,0 +1,45 @@
+/**
+ * Universidade do Estado do Rio de Janeiro
+ * Instituto de Matemática e Estatística
+ * Linguagem de Programação II
+ * profa. Priscilla Fonseca de Abreu Braz
+ * 
+ * Projeto: Implementação de cadastro em um Curso de Nível Superior
+ * 
+ * @author Matheus Costa Ramos Luiz
+ */
+package model;
+
+import java.util.ArrayList;
+
+
+/**
+ * A classe UsuarioManager representa um objeto que gerencia usuários.
+ */
+public class UsuarioManager {
+	
+	/**
+	 * A lista de usuários gerenciados.
+	 */
+    private ArrayList<UsuarioLogin> listaUsuarios;
+
+	/**
+	 * Cria um novo objeto UsuarioManager e inicializa a lista de usuários.
+	 */
+    public UsuarioManager() {
+        listaUsuarios = new ArrayList<>();
+        listaUsuarios.add(new UsuarioLogin("admPriscilla", "adm1"));
+        listaUsuarios.add(new UsuarioLogin("admMatheus", "adm2"));
+        listaUsuarios.add(new UsuarioLogin("paulo", "senha1"));
+    }
+
+	/**
+	 * Retorna a lista de usuários gerenciados.
+	 * 
+	 * @return A lista de usuários gerenciados.
+	 */
+    public ArrayList<UsuarioLogin> getListaUsuarios() {
+        return listaUsuarios;
+    }
+    
+}
